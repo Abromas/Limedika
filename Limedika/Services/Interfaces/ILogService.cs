69a24e60b2d0babda492b34c1fc37a49c6637cd5
Aivaras Abromas <aivaras.abromas@gmail.com>
2024-06-10 23:36:22 +0300

    Feature | #xxxxxx | add log page

--- Limedika/Services/Interfaces/ILogService.cs
+++ Limedika/Services/Interfaces/ILogService.cs
@@ -6,4 +6,5 @@ public interface ILogService
 {
     Task LogAsync(LogEntry logEntry);
     Task<IEnumerable<LogEntry>> GetLogsAsync();
+    Task<string> GetLogsAsStringWithSizeLimitAsync(int chunkSize);
 }
--- Limedika/Services/LogService.cs
+++ Limedika/Services/LogService.cs
@@ -24,6 +24,29 @@ public class LogService : ILogService
 
     public async Task<IEnumerable<LogEntry>> GetLogsAsync()
     {
-        return await _context.LogEntries.ToListAsync();
+        return await _context.LogEntries.OrderByDescending(x => x.Timestamp).Take(1000).ToListAsync();
+    }
+
+    public async Task<string> GetLogsAsStringWithSizeLimitAsync(int maxSizeInBytes)
+    {
+        var logs = await _context.LogEntries.OrderByDescending(x => x.Timestamp).ToListAsync();
+        var logString = new System.Text.StringBuilder();
+        var logSize = 0;
+
+        foreach (var log in logs)
+        {
+            var logEntryString = $"{log.Timestamp}\t{log.Action}\t{log.EntityName}\t{log.EntityId}\t{log.FieldName}\t{log.OldValue}\t{log.NewValue}{Environment.NewLine}";
+            var entrySize = System.Text.Encoding.UTF8.GetByteCount(logEntryString);
+
+            if (logSize + entrySize > maxSizeInBytes)
+            {
+                break;
+            }
+
+            logString.Append(logEntryString);
+            logSize += entrySize;
+        }
+
+        return logString.ToString();
     }
 }
